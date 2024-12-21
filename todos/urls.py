248dfec84405026cbb0402ef08455fbe48fce327
HarshitCodes2 <g1harshit7@gmail.com>
2024-12-21 19:15:56 +0530

    Change Directory Structure

--- todos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TodoListCreateView, TodoRetrieveUpdateDestroyView
-
-urlpatterns = [
-    path('', TodoListCreateView.as_view(), name='create_list_todo'),
-    path('<int:pk>/', TodoRetrieveUpdateDestroyView.as_view(), name='retrieve_update_destroy_todo'),
-]
